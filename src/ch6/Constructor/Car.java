29d025134774208052341114b8c7aee5a8e10cd8
gwangho-y <yigwangho21@gmail.com>
2023-06-03 19:33:14 +0900

    5.4 생성자에서 다른 생성자 호출 this

--- src/ch6/Constructor/Car.java
+++ src/ch6/Constructor/Car.java
@@ -5,7 +5,9 @@ public class Car {
     String gearType;
     int door;
 
-    Car() {}
+    Car() {
+        this("white", "auto", 4);
+    }
 
     Car(String c, String g, int d) {
         color = c;
@@ -14,7 +16,7 @@ public class Car {
     }
 
     Car(String color) {
-        door = 5;
-
+//        door = 5; // 다른 생성자 호출 전에 변수 값은 설정 못 한다. 다른 생성자에서 변수값을 수정해버리면 무의미해지는 경우가 생겨서 막아 놓은듯
+        this(color, "auto", 4); // 다른 생성자를 호출할 때는 this를 사용한다. 여기서 this는 Car(String c, String g, int d) 생성자를 호출한다.
     }
 }
--- src/ch6/Constructor/CarTest.java
+++ src/ch6/Constructor/CarTest.java
@@ -1,6 +1,6 @@
 package ch6.Constructor;
 
-public class ConstructorTest {
+public class CarTest {
 
     public static void main(String[] args) {
         Car c1 = new Car();
