60639f08e15c0f833db0d2f42753587512fcb57f
gwangho-y <yigwangho21@gmail.com>
2023-06-05 08:01:32 +0900

    ch7 1.1상송의 정의와 장점

---
+++ src/ch7/Tv/Tv.java
@@ -0,0 +1,12 @@
+package ch7.Tv;
+
+class Tv {
+    boolean power;
+    int channel;
+
+    void power() {power = !power;}
+    void channelUp() {++channel;}
+    void channelDown() {--channel;}
+
+
+}
