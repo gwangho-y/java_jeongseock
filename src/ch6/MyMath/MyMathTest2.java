9d09fcc56355073b6f23ae046baa6ff9bd14b4d5
gwangho-y <yigwangho21@gmail.com>
2023-07-26 09:06:58 +0900

    인스턴스와 클래스메서드

---
+++ src/ch6/MyMath/MyMathTest2.java
@@ -0,0 +1,14 @@
+package ch6.MyMath;
+
+class MyMath2 {
+    long a, b;
+    long add() {
+        return a + b;}
+    lon
+}
+
+class MyMathTest2 {
+    public static void main(String[] args) {
+
+    }
+}
