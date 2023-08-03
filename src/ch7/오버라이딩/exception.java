11dd844310e3888da9e5f2e0ebace0f11a3a3e01
gwangho-y <yigwangho21@gmail.com>
2023-08-03 09:07:23 +0900

    오버라이딩 조건

---
+++ src/ch7/오버라이딩/exception.java
@@ -0,0 +1,21 @@
+package ch7.오버라이딩;
+
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+class parent {
+    void parentMethod() throws IOException {
+
+    }
+}
+
+class child extends parent {
+//    void parentMethod() throws Exception {
+//
+//    }
+}
+
+public class exception {
+
+}
